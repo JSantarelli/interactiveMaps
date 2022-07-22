63bf00aee6db49cdae015b0243e7b8712e533e6a
Julio Nicolas Santarelli <jsantarelli@ABCNBK309.local>
2022-07-22 18:14:05 -0300

    feat(map): adds create operation

--- src/script.js
+++ src/script.js
@@ -23,9 +23,10 @@ const myObj = {
       return stadium.name;
     },
     facilities: [
-        {index: 1, name:"Estadio Mundialista", description: "Inaugurado en 1978, cuenta con una capacidad para 35.500 espectadores", sports:["Fúbol", "Atletismo", "Rugby"], year: 1978, positionX: 150, positionY: 300 },
-        {index: 2, name:"Estadio Islas Malvinas", description: "Fue inaugurado en 1995 con motivo de la realización de los XV Juegos Panamericanos", sports:["Tenis", "Voley", "Handbol"], year: 1995, positionX: 250, positionY: 500 },
-        {index: 3, name:"Natatorio Zorrilla", description: "Fue inaugurado en 1995 con motivo de la realización de los XV Juegos Panamericanos", sports:["Natación", "Clavadismo", "Waterpolo"], year: 1995, positionX: 300, positionY: 150 }
+        { name:"Estadio Mundialista", description: "Inaugurado en 1978, cuenta con una capacidad para 35.500 espectadores", sports:["Fúbol", "Atletismo", "Rugby"], year: 1978, anchor: "Stadium23" },
+        { name:"Estadio Islas Malvinas", description: "Fue inaugurado en 1995 con motivo de la realización de los XV Juegos Panamericanos", sports:["Tenis", "Voley", "Handbol"], year: 1995, anchor: "Shape15" },
+        { name:"Natatorio Zorrilla", description: "Fue inaugurado en 1995 con motivo de la realización de los XV Juegos Panamericanos", sports:["Natación", "Clavadismo", "Waterpolo"], year: 1995, anchor: "Nata202" },
+        { name:"Acceso Natatorio", description: "Capacidad para 20.000 espectadores", sports:["Natación", "Clavadismo", "Waterpolo"], year: 1995, anchor: "Nata203" }
     ]
   }
 
@@ -106,30 +107,14 @@ function createAnnotation(child) {
                     });
                     }
 
-                    // let point = document.getElementsByClassName("label");
-                    // let panel = document.getElementsByClassName("annotationDescription");
                     // const current = document.getElementById(aId);
-
-                    // Build-in toggle method
-                    // current[aId].className.toggle("active");
-                    
-                    // Replace method option
-                    // current.className = current.className.replace("annotationDescription", "active");
-
-                    // Iterative toggle
-                    // let i;
-                    // for(i = 0; i < panel.length; i++) {
-                    //     panel[i].addEventListener("click", togglePanel, false);
-                    // }
                         
                     // Conditional toggle
                     // if (current.style.display === "none") {
                     //     current.style.display = "block";
                     //   } else {
                     //     current.style.display = "none";
-                    // }
-                    // console.log(current);
-                
+                    // }                
                 }
           
                 if (annotationDisplay) {
@@ -144,7 +129,6 @@ function createAnnotation(child) {
                 annotations[aId].descriptionDomElement =
                     annotationTextDiv
                 }
-// }
 
 // SCENE
 const scene = new THREE.Scene()
@@ -160,17 +144,13 @@ loader.load( "/models/park.gltf", function ( gltf ) {
             const mesh = (child).clone();
 
             // Iteration through objects
+            let i;
 
-            if(mesh.name === "Stadium23") {
-                createAnnotation(child);
-            }
+            for (i = 0; i < myObj.facilities.length; i++) {
 
-            if(mesh.name === "Shape15") {
-                createAnnotation(child);
-            }
-
-            if(mesh.name === "Nata202") {
-                createAnnotation(child);
+                if(mesh.name === myObj.facilities[i].anchor) {
+                    createAnnotation(child);
+                }
             }
         }
     )
@@ -189,75 +169,116 @@ loader.load( "/models/park.gltf", function ( gltf ) {
 
 } );
 
-// OBJECTS
-const box = new THREE.BoxGeometry( 1, 1, 1 );
-const geometry = new THREE.SphereGeometry( .7, 20, 20, );
-const prisma = new THREE.BoxGeometry( 7, 3, .25);
-const cylinder = new THREE.CylinderGeometry( 1, 1, 2, 10 );
+// HTML
+// TO-DO: try bring elements using IDs, append, textContent, createNode, createElement  
 
-
-// cy.position.set( 2, 2, 1 )
-// scene.add(cy)
-
-// HTML | CSS3 labels
-// let renderer2 = new CSS3DRenderer();
-// renderer2.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer2.domElement);
-
-// let el = document.createElement('div');
-// el.innerHTML = "<span class='style'>Hola!</span>";
-// let obj = new CSS3DObject(el);
-// obj.position.set(0,0,0);
-// scene.add(obj);
-
-// function draw() {
-//     renderer2.render(scene, camera);
-//     requestAnimationFrame(draw)
+// for (let i in myObj.facilities) {
+    //     const para = document.createElement("p");
+    //     const node = document.createTextNode(myObj.facilities[i].name);
+    //     node.domElement.style.color = 'blue';
+    //     para.className = 'holder';
+    //     para.appendChild(node);
+    //     const element = document.getElementById("holder");
+    //     element.appendChild(para);
 // }
 
-// Align Scene to HTML
-// const wrapper = document.querySelector('#c');
-// const renderer3 = new THREE.WebGLRenderer({wrapper});
+let i;
 
-
-
-// CSS2 Labels | https://github.com/mrdoob/three.js/blob/master/examples/css2d_label.html
-const EARTH_RADIUS = 1;
-const earthGeometry = new THREE.SphereGeometry( EARTH_RADIUS, 16, 16 );
-const earthMaterial = new THREE.MeshPhongMaterial( {
-    specular: 0x333333,
-    shininess: 5,
-    map: new THREE.TextureLoader().load("/img/textWater.jpg"),
-    normalScale: new THREE.Vector2( 0.85, 0.85 )
-} );
-const earth = new THREE.Mesh( earthGeometry, earthMaterial );
-
-// scene.add( earth );
-
-
-function init() {
-    const earthMassDiv = document.createElement( 'div' );
-
-    earthMassDiv.innerHTML = '<span>Hola!</span>';
-
-    earthMassDiv.className = 'label';
-    earthMassDiv.textContent = 'Annotation created with CSS2D Library';
-    earthMassDiv.style.marginTop = '-1em';
-    const earthMassLabel = new CSS2DObject( earthMassDiv );
-    earthMassLabel.position.set( -1700, 25, 50 );
-    earth.add( earthMassLabel );
-    earthMassLabel.layers.set( 1 );
-    // console.log(earthMassDiv)
-
+function binding() {
+    // Add code block that updates the map!
+    console.log("map updated!")
 }
 
-// Annotation renderer
-// let annotationRenderer = new CSS2DRenderer()
-// annotationRenderer.setSize( window.innerWidth, window.innerHeight )
-// annotationRenderer.domElement.style.position = 'absolute'
-// annotationRenderer.domElement.style.top = '0px'
-// document.body.appendChild( annotationRenderer.domElement )
+// Adding facilities
+function addClient() {
+    
+    var newFacility = {
+      name:document.getElementById("facility-name").value,
+      year:document.getElementById("facility-year").value,
+      description:document.getElementById("facility-desc").value,
+      sports:document.getElementById("facility-sports").value,
+      anchor:document.getElementById("facility-anchor").value,
+    }
+    
+    myObj.facilities.push(newFacility)
+    displayClients()
+    binding()
+  }
 
+const btn = document.getElementById("btn-save");
+btn.addEventListener('click', addClient)
+
+// Displaying current facilities
+function displayClients() {    
+    document.getElementById("list").innerHTML = " "
+
+    for (i = 0; i < myObj.facilities.length; i++) {
+        
+      var myTr = document.createElement("tr")
+      console.log(myTr)
+
+      for(let a in myObj.facilities[i]) {
+        var myTd = document.createElement("td")
+        // myTd.className = "floating"
+        myTd.innerHTML = myObj.facilities[i][a]
+        myTr.appendChild(myTd)
+        console.log(myTd)
+      }
+      
+        var myTd = document.createElement("td")
+        myTd.innerHTML = myObj.facilities[i].name
+        myTr.appendChild(myTd)
+
+        var actionTd = document.createElement("td")
+        var editBtn = document.createElement("button")
+        editBtn.textContent = "Edit"
+        editBtn.setAttribute("class" , "btn btn-sm btn-primary")
+        editBtn.setAttribute("onclick" , "editClient(" + i + ")")
+
+        var deletebtn = document.createElement("button")
+        deletebtn.innerHTML="Delete"
+        deletebtn.setAttribute("class" , "btn btn-sm btn-danger")
+        deletebtn.setAttribute("onclick" , "deleteClient(" + i + ")")
+
+        actionTd.appendChild(editBtn)
+        actionTd.appendChild(deletebtn)
+        myTr.appendChild(actionTd)
+        document.getElementById("list").appendChild(myTr)
+      }
+    
+      document.getElementById("facility-name").value = " ";
+      document.getElementById("facility-year").value = " ";
+      document.getElementById("facility-desc").value = " ";
+      document.getElementById("facility-sports").value = " ";
+      document.getElementById("facility-anchor").value = " "
+      
+    }
+
+  displayClients()
+
+
+// CreateNode method
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('holder');
+
+// for (let i in myObj.facilities) {
+    // Creating a table
+//     let li = createNode('li');
+//     let img = createNode('img');
+//     let span = createNode('span');
+//     li.innerHTML = myObj.facilities[i].name;
+//     li.className = 'holder';
+//     append(li, img);
+//     append(li, span);
+//     append(ul, li);
+// }
 
 // Renderer for 2D labels
 let labelRenderer = new CSS2DRenderer();
@@ -266,41 +287,43 @@ labelRenderer.domElement.style.position = 'absolute';
 labelRenderer.domElement.style.top = '0px';
 document.body.appendChild( labelRenderer.domElement );
 
+
+// OBJECTS
+const geometry = new THREE.SphereGeometry( .7, 20, 20, );
+
+const labelContainerElem = document.querySelector('#labels');
+
+function makeInstance(geometry, color, x, name) {
+    const material = new THREE.MeshPhongMaterial({color});
+
+    const form = new THREE.Mesh(geometry, material);
+    scene.add(form);
+
+    form.position.x = x;
+
+    const elem = document.createElement('h2');
+    elem.textContent = name;
+    labelContainerElem.appendChild(elem);
+
+
+    return {form, elem};
+}
+
+// Prototypes
+const cubes = [
+    makeInstance(geometry, 0x44aa88,  0, 'Cube'),
+];
+
+
 // MATERIALS
-// const textureLoader = new THREE.textureLoader()
-const skin = new THREE.TextureLoader().load("/img/textWater.jpg")
-const texture = new THREE.TextureLoader().load("/img/matcap-gold.png")
-
-const material = new THREE.MeshStandardMaterial
-material.metalness = 0.7
-material.roughness = 0.2
-
 const material2 = new THREE.MeshLambertMaterial({color: 0xfff000});
-const material3 = new THREE.MeshPhongMaterial({color: 0xff0000})
+
 
 // Texturing
-material.normalMap = texture;
-material3.normalMap = skin;
-const toon = new THREE.MeshToonMaterial()
 
-material.color = new THREE.Color(0xffffff)
-material2.color = new THREE.Color(0xe7e7e7)
 
 // MESH
-const sphere = new THREE.Mesh(geometry,material)
-// scene.add(sphere)
 
-const cube = new THREE.Mesh(box, material2)
-cube.position.x = 1
-cube.position.y = 2
-cube.position.z = -1
-// scene.add(cube)
-
-const panel = new THREE.Mesh(prisma, material2)
-panel.position.set( -1, 2, 2 )
-// scene.add(panel)
-
-const cy = new THREE.Mesh(cylinder, material3)
 
 // LIGHTS
 const pointLight1 = new THREE.PointLight(0xffffff, 2)
@@ -327,29 +350,11 @@ const sizes = {
     height: window.innerHeight
 }
 
-// window.addEventListener('resize', () =>
-// {
-//     // Update sizes
-//     sizes.width = window.innerWidth
-//     sizes.height = window.innerHeight
-
-//     // Update camera
-//     camera.aspect = sizes.width / sizes.height
-//     camera.updateProjectionMatrix()
-
-//     // Update renderer
-//     renderer.setSize(sizes.width, sizes.height)
-//     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// })
-
 /**
  * CAMERA
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(100, sizes.width / sizes.height, 0.1, 2000)
-// camera.position.x = 0
-// camera.position.y = 0
-// camera.position.z = 2
 camera.position.set( -1900, 500, 900)
 
 scene.add(camera)
@@ -363,15 +368,6 @@ controls.enableDamping = true
 // INTERACTIVITY
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
-
-// Change property based on event
-// document.body.addEventListener("click", hola)
-
-
-// function hola() {
-//     document.getElementById("demo").innerHTML = "Hola!";
-//     console.log('hola!')
-// }
 
 // based on interaction with object
 function onMouseMove(event) {
@@ -396,37 +392,7 @@ function onMouseMove(event) {
 
         document.getElementById("demo").innerHTML = "Object color is " + this.temp
         
-        // Loop object
-        // var meshX = -10;
-
-        // Create random objects
-        // for(var i = 0; i < 10; i++) {
-        //     var mesh2 = new THREE.Mesh(box, material3);
-        //     mesh2.position.x = (Math.random() -0.5) * 10;
-        //     mesh2.position.y = (Math.random() -0.5) * 10;
-        //     mesh2.position.z = (Math.random() -0.5) * 10;
-        //     scene.add(mesh2);
-        //     meshX += 1;
-        // }
-
-        // Change property
-        // intersects[i].object.material.color.set(0xff0000)
-
-        // Adding interaction
-        // this.tl = new TimelineMax({paused: true});
-        // this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut})
-        
-        // document.body.addEventListener('click', () => {
-        //     this.tl.play();
-        // })
-        
         this.tl = new TimelineMax().delay(.3);
-
-        // this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut})
-        // this.tl.to(intersects[i].object.scale, .5, {x: .5, ease: Expo.easeOut})
-
-        // this.tl.to(intersects[i].object.position, .5, {x: 2, ease: Expo.easeOut})
-        // this.tl.to(intersects[i].object.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, "=-1.5")
     }
 
 }
@@ -463,47 +429,18 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * ANIMATIONS
  */
-// Static Labels
-const labelContainerElem = document.querySelector('#labels');
 
-function makeInstance(geometry, color, x, name) {
-    const material = new THREE.MeshPhongMaterial({color});
-
-    const form = new THREE.Mesh(geometry, material);
-    scene.add(form);
-
-    form.position.x = x;
-
-    const elem = document.createElement('h2');
-    elem.textContent = name;
-    labelContainerElem.appendChild(elem);
-
-
-    return {form, elem};
-}
-
-const cubes = [
-    // makeInstance(geometry, 0x44aa88,  0, 'Cube'),
-    // makeInstance(box, 0x8844aa, -2, 'Sphere'),
-    // makeInstance(cylinder, 0xaa8844,  2, 'Cylinder'),
-];
 
 // based on a function
 var render = function() {
     requestAnimationFrame(render);
-
-    cube.rotation.y += 0.01;
-    panel.lookAt( camera.position );
-
+    
     renderer.render(scene, camera)
 }
 
 render();
-// draw();
-init();
 
 const clock = new THREE.Clock()
-
 
 const tick = () =>
 {
@@ -512,17 +449,6 @@ const tick = () =>
     targetY = mouseY * .001
 
     const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    // sphere.rotation.y = .5 * elapsedTime
-
-    // sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
-    // sphere.rotation.z += .5 * (targetY - sphere.rotation.z)
-    
-    // Update Orbital Controls
-    // controls.update()
-
-    
 
     // Render
     renderer.render(scene, camera);
