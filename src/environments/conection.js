138424af19a75a8f834b6f80eae3c680926c48ad
Julio Nicolás Santarelli <santarellijulio@gmail.com>
2024-01-26 16:06:14 -0300

    Update conection.js

--- src/environments/conection.js
+++ src/environments/conection.js
@@ -1,10 +1,10 @@
 // Firebase configuration
 export const firebaseConfig = {
-    apiKey: "AIzaSyDEslhlteP9FzDmI9ax3p-rwwAMALW6XHA",
+    apiKey: "",
     authDomain: "intermaptive.firebaseapp.com",
     projectId: "intermaptive",
     storageBucket: "intermaptive.appspot.com",
-    messagingSenderId: "260919370396",
-    appId: "1:260919370396:web:583746f34afe4ae4f34359"
+    messagingSenderId: "",
+    appId: ""
     };
     
